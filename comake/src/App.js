9cbeb16298a684f818f6757003039765dd29c8f5
nelson-singleton <nsingleton@ymail.com>
2020-09-21 18:14:20 -0500

    Push test

--- comake/src/App.js
+++ comake/src/App.js
@@ -8,30 +8,12 @@ import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch, useParams 
 
 function App() {
   return (
- //nelson-singleton
+ 
     <div className="App">
       <LandingPage />
+</div>
 
-    <div>
-      <AuthPage></AuthPage>
- //master
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+ 
   );
 }
 
