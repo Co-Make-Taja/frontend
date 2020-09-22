1f9c9a4cf54cd751a4d4610e07efca8ec06476fe
nelson-singleton <nsingleton@ymail.com>
2020-09-22 12:09:00 -0500

    Created update issue form and functionality

--- comake/src/components/newissue.js
+++ comake/src/components/newissue.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import { useParams } from "react-router-dom";
+import {useHistory} from 'react-router-dom'
 
 export function Issues(){
     return(
---
+++ comake/src/components/updateissue.js
@@ -0,0 +1,86 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import {useHistory, useParams} from 'react-router-dom'
+
+import {Issues} from './newissue'
+
+const initialIssue = {
+    issueid: "",
+    title: "Issue title",
+    description: "Having an issue",
+    image: "Image url here",
+    category: {},
+    user: {},
+    comments: []
+}
+
+export const updateIssue = (props) => {
+    const history = useHistory()
+    const {id} = useParams()
+    const [someIssue, setSomeIssue] = useState(initialIssue)
+}
+
+const handleChanges = e => {
+    e.preventDefault()
+    setSomeIssue({
+        ...someIssue,
+            [e.target.name]: e.target.value
+    })
+}
+
+useEffect(() => {
+    axios
+        .get(`https://bw-comakeapp-java.herokuapp.com/issues/issues/${id}`)
+        .then(res => {
+            console.log(res, "Getting specific issue")
+            setSomeIssue(res.data)
+        })
+},[id])
+
+submitChange (() => {
+    e.preventDefault()
+    axios
+        .put(`https://bw-comakeapp-java.herokuapp.com/issues/issues/${id}`)
+        .then(res => {
+            console.log(res, "Put request to update issue")
+            setSomeIssue(initialIssue)
+            history.push(`/dashboard/${id}`)
+        })
+})
+
+return ( 
+    <form onSubmit = {submitChange}>
+        <input
+        type = 'text'
+        name = 'title'
+        onChange = {handleChanges}
+        value = {someIssue.title}
+        placeholder = "Issue Titles"
+        />
+
+        <input
+        type = 'text'
+        name = 'description'
+        onChange = {handleChanges}
+        value = {someIssue.description}
+        placeholder = "Issue Description"
+        />
+
+        <input
+        type = 'text'
+        name = 'title'
+        onChange = {handleChanges}
+        value = {someIssue.title}
+        placeholder = "Issue Titles"
+        />
+
+        <input
+        type = 'text'
+        name = 'image'
+        onChange = {handleChanges}
+        value = {someIssue.image}
+        placeholder = "Image of issue"
+        />
+        <button>Submit</button>
+    </form>
+)
